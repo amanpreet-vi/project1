a71f5f82b12efa1cb6cb44f36e72fe3149a20658
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-07 06:45:40 +0530

    some pages added

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (person) => {
+  return (
+    <div className="card">
+      <h1 className="heading">this is card component</h1>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App"; not used here
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>{/* <App />  not used here*/}</React.StrictMode>);
