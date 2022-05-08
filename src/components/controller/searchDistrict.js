e839d56285c713238bd6a3c2dec1f4cfa1b462d7
Shivam Yadav <shivay9455@gmail.com>
2022-05-08 21:23:25 +0530

    search district implemented

---
+++ src/components/controller/searchDistrict.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+import { API } from '../../backend';
+
+export default function searchDistrict(district) {
+	return fetch(`${API}/up/district/${district}`, { method: 'GET' })
+		.then((res) => {
+			return res.json();
+		})
+		.catch(console.log);
+}
