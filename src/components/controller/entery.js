baa3a0ea6131b5691e3316a8ee5de07e0e957f48
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-08 08:36:38 +0530

    haha some signin signUp changes

--- src/components/controller/entery.js
+++ src/components/controller/entery.js
@@ -1,39 +1,57 @@
 import React from "react";
 import SignIn from "./signIn";
 import SignUp from "./signUp";
+import "../resource/stylesheet/entry.css";
 
 export default function Entry() {
-  const [signIn, setSignIn] = React.useState("inactive");
+  const [signIn, setSignIn] = React.useState("active");
+
+  function change() {
+    var sign_in = document.getElementById("signIn");
+    var sign_up = document.getElementById("signUp");
+    if (signIn === "active") {
+      sign_in.style.backgroundColor("red");
+    } else {
+      sign_up.style.backgroundColor("red");
+    }
+  }
 
   return (
     <>
       <div
         style={{
           width: "98vw",
-          height: "100vh",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           flexDirection: "column",
         }}
       >
-        <div>
-          <button
-            onClick={() => {
-              setSignIn("active");
-            }}
-          >
-            Sign In
-          </button>
-          <button
-            onClick={() => {
-              setSignIn("inactive");
-            }}
-          >
-            Sign Up
-          </button>
+        <div className="buttonContainer ">
+          <div>
+            <button
+              id={`${signIn}`}
+              onClick={() => {
+                setSignIn("active");
+                change();
+              }}
+            >
+              Sign In
+            </button>
+            <button
+              id={`${signIn === "active" ? "inactive" : "active"}`}
+              onClick={() => {
+                setSignIn("inactive");
+                change();
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
-        <div>{signIn === "active" ? <SignIn /> : <SignUp />}</div>
+        <div style={{ padding: "20vh 0vw" }}>
+          <div>{signIn === "active" ? <SignIn /> : <SignUp />}</div>
+        </div>
       </div>
     </>
   );
