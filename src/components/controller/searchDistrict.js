a6ed3f2864c9da681c4627dcdda1fe45e76dd401
Shivam Yadav <shivay9455@gmail.com>
2022-05-26 12:54:59 +0530

    added logic for card component

--- src/components/controller/searchDistrict.js
+++ src/components/controller/searchDistrict.js
@@ -1,13 +1,12 @@
 /** @format */
 
 // import React from 'react';
-import { API } from "../../backend";
+import { API } from '../../backend';
 
 export default function searchDistrict(district) {
-  return fetch(`${API}/up/district/${district}`, { method: "GET" })
-    .then((res) => {
-      console.log("in");
-      return res.json();
-    })
-    .catch(console.log);
+	return fetch(`${API}/up/district/${district}`, { method: 'GET' })
+		.then((res) => {
+			return res.json();
+		})
+		.catch(console.log);
 }
--- src/components/view/card.jsx
+++ src/components/view/card.jsx
@@ -1,18 +1,24 @@
 /** @format */
 
-import React from "react";
-import "../resource/stylesheet/card.css";
+import React from 'react';
+import '../resource/stylesheet/card.css';
 
 export default function Cards({ district }) {
-  return (
-    <div className="card-container">
-      <h2>{district.name}</h2>
-      <h3>Constituency:</h3>
-      <ul>
-        {district.constituency.map((c, i) => (
-          <li key={i}>{c}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<>
+			{district ? (
+				<div className="card-container">
+					<h2>{district.name}</h2>
+					<h3>Constituency:</h3>
+					<ul>
+						{district.constituency.map((c, i) => (
+							<li key={i}>{c}</li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<></>
+			)}
+		</>
+	);
 }
