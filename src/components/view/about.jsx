2a35de0cf64fd5b63f9bd8560b1962c5db8b2fe8
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-27 22:52:35 +0530

    about finalized

--- src/components/view/about.jsx
+++ src/components/view/about.jsx
@@ -5,6 +5,7 @@ export default function About() {
     <>
       <div className="aboutContainer">
         <h1>About Us</h1>
+        <hr />
         <p>
           Sometimes regular voter doesn't know about their current MLA/MP's past
           and their educational background. And what make it worse is voter can
@@ -13,6 +14,20 @@ export default function About() {
           information about candidate to voters. Votechain give user to ability
           to review candidate's work in their area.
         </p>
+        <hr />
+        <p>
+          To give user ability to think about choices to make in next election
+          by going through the data provided by Votechain about candidates.
+          Voter can help each other by providing genuine review about
+          candidates.
+        </p>
+        <p>&nbsp;</p>
+        <p>
+          Our vision is to create the voting mechanism on the blockchain
+          technology with MERN stack so that, every vote would be secure and
+          safe from all of the vulnerabilities.
+        </p>
+        <hr />
       </div>
     </>
   );
