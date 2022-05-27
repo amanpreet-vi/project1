3971eb1e593805a838abfb07da5186682be3b645
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-27 20:18:51 +0530

    admincontroller

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 /** @format */
 
-import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import LandingPage from './components/controller/landingPage';
-import SignIn from './components/controller/signIn';
-import SignUp from './components/controller/signUp';
-import AdminPanelView from './components/view/adminPanelView';
-import NavBar from './components/view/navBar';
-import './components/resource/stylesheet/app.css';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./components/controller/landingPage";
+import SignIn from "./components/controller/signIn";
+import SignUp from "./components/controller/signUp";
+// import AdminPanelView from "./components/view/adminPanelView";
+import NavBar from "./components/view/navBar";
+import "./components/resource/stylesheet/app.css";
 
-import CandidateController from './components/controller/candidateController';
-import { isAuthenticated } from './components/controller/authentication';
+import CandidateController from "./components/controller/candidateController";
+import { isAuthenticated } from "./components/controller/authentication";
+import AdminController from "./components/controller/adminController";
 // import CardController from './components/controller/cardController';
 
 export default function App() {
-	const [render, setRender] = useState(false);
+  const [render, setRender] = useState(false);
 
-	return (
-		<>
-			<NavBar setRender={setRender} />
-			<div className="base">
-				<Switch>
-					<Route path="/" exact>
-						<LandingPage />
-					</Route>
-					<Route path="/signin">
-						<SignIn setRender={setRender} />
-					</Route>
-					<Route path="/signup">
-						<SignUp setRender={setRender} />
-					</Route>
-					<Route path="/candidate">
-						<CandidateController />
-					</Route>
-					<Route path="/admin">
-						<AdminPanelView />
-					</Route>
-				</Switch>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <NavBar setRender={setRender} />
+      <div className="base">
+        <Switch>
+          <Route path="/" exact>
+            <LandingPage />
+          </Route>
+          <Route path="/signin">
+            <SignIn setRender={setRender} />
+          </Route>
+          <Route path="/signup">
+            <SignUp setRender={setRender} />
+          </Route>
+          <Route path="/candidate">
+            <CandidateController />
+          </Route>
+          <Route path="/admin">
+            <AdminController />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
 }
