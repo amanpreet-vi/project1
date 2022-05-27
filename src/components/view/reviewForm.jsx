e0d118275c9f8c7fb1656ed56d1b99f80953ab5d
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-28 00:17:36 +0530

    reviewForm style updated

--- src/components/view/reviewForm.jsx
+++ src/components/view/reviewForm.jsx
@@ -1,34 +1,33 @@
 /** @format */
-
-import React from 'react';
+import "../resource/stylesheet/reviewForm.css";
+import React from "react";
 
 export default function ReviewForm({ handlechange, onSubmit, errorMessage }) {
-	return (
-		<>
-			<div>
-				<form target="#">
-					<select
-						name="rating"
-						onChange={handlechange('rating')}
-						id="rating"
-					>
-						<option value="0">0</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-					</select>
-					<textarea
-						name="comment"
-						onChange={handlechange('comment')}
-					></textarea>
-
-					<button onClick={onSubmit} type="submit">
-						Submit
-					</button>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="reviewForm">
+        <form target="#">
+          <div>
+            <select name="rating" onChange={handlechange("rating")} id="rating">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <textarea
+              name="comment"
+              onChange={handlechange("comment")}
+            ></textarea>
+          </div>
+          <div>
+            <button onClick={onSubmit} type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
