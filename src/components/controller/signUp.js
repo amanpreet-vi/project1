7d89a16a7ebf3471a639c722e40cbc62137ab84c
Shivam Yadav <shivay9455@gmail.com>
2022-05-27 18:45:21 +0530

    added pancard validation

--- src/components/controller/signUp.js
+++ src/components/controller/signUp.js
@@ -1,86 +1,102 @@
 /** @format */
 
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import SignUpView from "../view/signUpView";
-import { signup } from "./authentication";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import SignUpView from '../view/signUpView';
+import { signup } from './authentication';
 
 export default function SignUp() {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-    pancard: "",
-    error: "",
-    success: false,
-  });
-  const { name, email, password, pancard, error, success } = values;
+	const [values, setValues] = useState({
+		name: '',
+		email: '',
+		password: '',
+		pancard: '',
+		error: '',
+		success: false,
+	});
+	const { name, email, password, pancard, error, success } = values;
 
-  const handleChange = (name) => (event) => {
-    setValues({ ...values, error: false, [name]: event.target.value });
-  };
+	const handleChange = (name) => (event) => {
+		if (name === 'pancard') {
+			if (pancardValidation(event.target.value.toUpperCase())) {
+				setValues({
+					...values,
+					error: false,
+					[name]: event.target.value,
+				});
+			}
+		} else
+			setValues({ ...values, error: false, [name]: event.target.value });
+	};
 
-  const onSubmit = (event) => {
-    console.log("in");
-    event.preventDefault();
-    setValues({ ...values, error: false });
-    signup({ name, email, pancard, password })
-      .then((data) => {
-        if (data.error) {
-          setValues({ ...values, error: data.error, success: false });
-          console.log(values);
-        } else {
-          setValues({
-            ...values,
-            name: "",
-            email: "",
-            password: "",
-            pancard: "",
-            error: "",
-            success: true,
-          });
-        }
-      })
-      .catch((err) => console.log("Error in signup route"));
-  };
+	const onSubmit = (event) => {
+		event.preventDefault();
+		setValues({ ...values, error: false });
+		signup({ name, email, pancard, password })
+			.then((data) => {
+				if (data.error) {
+					setValues({ ...values, error: data.error, success: false });
+				} else {
+					setValues({
+						...values,
+						name: '',
+						email: '',
+						password: '',
+						pancard: '',
+						error: '',
+						success: true,
+					});
+				}
+			})
+			.catch((err) => console.log('Error in signup route'));
+	};
 
-  const successMessage = () => {
-    return (
-      <div className="row">
-        <div className="col-md-6 offset-sm-3 text-left">
-          <div
-            className="alert alert-success"
-            style={{ display: success ? "" : "none" }}
-          >
-            Welcome. Please <Link to="/signin">Login here.</Link>
-          </div>
-        </div>
-      </div>
-    );
-  };
-  const errorMessage = () => {
-    return (
-      <div className="row">
-        <div className="col-md-6 offset-sm-3 text-left">
-          <div
-            className="alert alert-danger"
-            style={{ display: error ? "" : "none" }}
-          >
-            {error}
-          </div>
-        </div>
-      </div>
-    );
-  };
+	const pancardValidation = (pancard) => {
+		console.log(pancard);
+		let regex = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/;
 
-  return (
-    <>
-      <SignUpView
-        handleChange={handleChange}
-        onSubmit={onSubmit}
-        errorMessage={errorMessage}
-        successMessage={successMessage}
-      />
-    </>
-  );
+		if (!regex.test(pancard)) {
+			setValues({ ...values, error: 'Invalid Pan card Number' });
+		} else return true;
+	};
+
+	const successMessage = () => {
+		return (
+			<div className="row">
+				<div className="col-md-6 offset-sm-3 text-left">
+					<div
+						className="alert alert-success"
+						style={{ display: success ? '' : 'none' }}
+					>
+						Welcome. Please <Link to="/signin">Login here.</Link>
+					</div>
+				</div>
+			</div>
+		);
+	};
+	const errorMessage = () => {
+		return (
+			<div className="row">
+				<div className="col-md-6 offset-sm-3 text-left">
+					<div
+						className="alert alert-danger"
+						style={{ display: error ? '' : 'none' }}
+					>
+						{error}
+					</div>
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<>
+			<SignUpView
+				handleChange={handleChange}
+				onSubmit={onSubmit}
+				errorMessage={errorMessage}
+				successMessage={successMessage}
+			/>
+		</>
+	);
 }
