5336e1bc79c7a7cccb575909b0e66802ba18e741
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-27 22:39:47 +0530

    about

--- src/components/view/navBar.jsx
+++ src/components/view/navBar.jsx
@@ -1,59 +1,61 @@
 /** @format */
 
-import React, { Fragment, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { isAuthenticated, signout } from '../controller/authentication';
-import '../resource/stylesheet/navbar.css';
+import React, { Fragment, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { isAuthenticated, signout } from "../controller/authentication";
+import "../resource/stylesheet/navbar.css";
 
 export default function NavBar({ setRender }) {
-	let history = useHistory();
-	return (
-		<div className="nav">
-			<div className="navContainer">
-				<a className="title" href="/">
-					<div>
-						Vote<span>chain</span>
-					</div>
-				</a>
-				<div className="hamBurger">
-					<div></div>
-					<div></div>
-					<div></div>
-				</div>
-				<div className="links">
-					<div>about</div>
-					<div>contact us</div>
+  let history = useHistory();
+  return (
+    <div className="nav">
+      <div className="navContainer">
+        <a className="title" href="/">
+          <div>
+            Vote<span>chain</span>
+          </div>
+        </a>
+        <div className="hamBurger">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="links">
+          <a href="/about">
+            <div>about</div>
+          </a>
+          <div>contact us</div>
 
-					{!isAuthenticated() && (
-						<Fragment>
-							<div>
-								<Link className="nav-link" to="/signup">
-									Sign Up
-								</Link>
-							</div>
-							<div>
-								<Link className="nav-link" to="/signin">
-									Sign In
-								</Link>
-							</div>
-						</Fragment>
-					)}
-					{isAuthenticated() && (
-						<div>
-							<span
-								onClick={() => {
-									signout(() => {
-										setRender(false);
-										history.push('/');
-									});
-								}}
-							>
-								Sign Out
-							</span>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+          {!isAuthenticated() && (
+            <Fragment>
+              <div>
+                <Link className="nav-link" to="/signup">
+                  Sign Up
+                </Link>
+              </div>
+              <div>
+                <Link className="nav-link" to="/signin">
+                  Sign In
+                </Link>
+              </div>
+            </Fragment>
+          )}
+          {isAuthenticated() && (
+            <div>
+              <span
+                onClick={() => {
+                  signout(() => {
+                    setRender(false);
+                    history.push("/");
+                  });
+                }}
+              >
+                Sign Out
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
