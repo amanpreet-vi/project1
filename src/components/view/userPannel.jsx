d2a68b9fdae5ccaebcce78bcd6d597f8e9b60cfc
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-29 14:41:09 +0530

    user pannel ui change

--- src/components/view/userPannel.jsx
+++ src/components/view/userPannel.jsx
@@ -1,49 +1,49 @@
 /** @format */
 
-import { useState } from 'react';
-import { isAuthenticated } from '../controller/authentication';
-import { deleteReview } from '../controller/reviewApiCall';
-import '../resource/stylesheet/userPannel.css';
+import { useState } from "react";
+import { isAuthenticated } from "../controller/authentication";
+import { deleteReview } from "../controller/reviewApiCall";
+import "../resource/stylesheet/userPannel.css";
 export default function UserPannel({ reviews, setDidRedirect }) {
-	const {
-		token,
-		user: { name, email, pancard, _id },
-	} = isAuthenticated();
+  const {
+    token,
+    user: { name, email, pancard, _id },
+  } = isAuthenticated();
 
-	const handleOnclick = (review) => (event) => {
-		event.preventDefault();
-		deleteReview(token, review.candidate, review._id, _id).then(() => {
-			setDidRedirect((prev) => !prev);
-		});
-	};
-	return (
-		<>
-			<div className="userPannelContainer">
-				<div className="left">
-					<h1>Candidate Info</h1>
-					<div>Name: {name.toUpperCase()} </div>
-					<div>PanNumber: {pancard} </div>
-					<div>Email: {email}</div>
-				</div>
-				<div className="beech"></div>
-				<div className="right">
-					<h1 style={{ margin: 'auto' }}>Reviews</h1>
-					{reviews.map((review) => {
-						return (
-							<div>
-								<div>
-									<span>Rating: </span>
-									{review.rating}
-								</div>
-								<div>{review.comment}</div>
-								<button onClick={handleOnclick(review)}>
-									Delete
-								</button>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</>
-	);
+  const handleOnclick = (review) => (event) => {
+    event.preventDefault();
+    deleteReview(token, review.candidate, review._id, _id).then(() => {
+      setDidRedirect((prev) => !prev);
+    });
+  };
+  return (
+    <>
+      <div className="userPannelContainer">
+        <div className="left">
+          <h1>Candidate Info</h1>
+          <div>Name: {name.toUpperCase()} </div>
+          <div>PanNumber: {pancard} </div>
+          <div>Email: {email}</div>
+        </div>
+        <div className="beech"></div>
+        <div className="right">
+          <h1>Reviews</h1>
+          <div className="reviewList">
+            {reviews.map((review) => {
+              return (
+                <div className="reviewBox">
+                  <div>
+                    <span>Rating: </span>
+                    {review.rating}
+                  </div>
+                  <div>{review.comment}</div>
+                  <button onClick={handleOnclick(review)}>Delete</button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
