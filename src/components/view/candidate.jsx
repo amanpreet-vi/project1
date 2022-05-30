c0567c46f9c7430b1638a81008f76ecd6e97c4e9
amanpreet-vi <amanpreet3338@gmail.com>
2022-05-31 04:14:54 +0530

    review form update

--- src/components/view/candidate.jsx
+++ src/components/view/candidate.jsx
@@ -1,41 +1,38 @@
 /** @format */
 
-import React, { useState } from 'react';
-import { isAuthenticated } from '../controller/authentication';
-import CreateReviewForm from '../controller/createReviewForm';
+import React, { useState } from "react";
+import { isAuthenticated } from "../controller/authentication";
+import CreateReviewForm from "../controller/createReviewForm";
 
-import '../resource/stylesheet/candidate.css';
-import Info from './info';
-import News from './news';
-import Rating from './rating';
-import Review from './review';
+import "../resource/stylesheet/candidate.css";
+import Info from "./info";
+import News from "./news";
+import Rating from "./rating";
+import Review from "./review";
 
 export default function Candidate({ candidate }) {
-	return (
-		<>
-			<div className="candidateContainer">
-				<div className="top">
-					<Info candidate={candidate} />
-					<News newsArticles={candidate.newsArticles} />
-				</div>
-				<div className="bottom">
-					<Rating stars={candidate.ratings} />
-					{isAuthenticated() ? (
-						<CreateReviewForm
-							candidateID={candidate._id}
-							userID={isAuthenticated().user._id}
-							token={isAuthenticated().token}
-						/>
-					) : (
-						<></>
-					)}
+  return (
+    <>
+      <div className="candidateContainer">
+        <div className="top">
+          <Info candidate={candidate} />
+          <News newsArticles={candidate.newsArticles} />
+        </div>
+        <div className="bottom">
+          <Rating stars={candidate.ratings} />
+          {isAuthenticated() ? (
+            <CreateReviewForm
+              candidateID={candidate._id}
+              userID={isAuthenticated().user._id}
+              token={isAuthenticated().token}
+            />
+          ) : (
+            <></>
+          )}
 
-					<Review
-						reviews={candidate.reviews}
-						candidateID={candidate._id}
-					/>
-				</div>
-			</div>
-		</>
-	);
+          <Review reviews={candidate.reviews} candidateID={candidate._id} />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/view/candidateupdateform.jsx
+++ src/components/view/candidateupdateform.jsx
@@ -1,79 +1,87 @@
 /** @format */
-import '../resource/stylesheet/reviewForm.css';
-import React, { useState } from 'react';
+import "../resource/stylesheet/candidateUpdateForm.css";
+import React, { useState } from "react";
 
 export default function CandidateUpdate(props) {
-	const [candidate, setCandidate] = useState(props.location.state);
+  const [candidate, setCandidate] = useState(props.location.state);
 
-	const handleChange = (name) => (event) => {
-		setCandidate({
-			...candidate,
-			[name]: event.target.value,
-		});
-	};
+  const handleChange = (name) => (event) => {
+    setCandidate({
+      ...candidate,
+      [name]: event.target.value,
+    });
+  };
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		setCandidate({ ...candidate });
-	};
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setCandidate({ ...candidate });
+  };
 
-	return (
-		<>
-			<div className="">
-				<form target="#">
-					<div>
-						<label htmlFor="Name">Name: </label>
-						<input
-							type="text"
-							name="Name"
-							value={candidate.name}
-							onChange={handleChange('name')}
-						/>
-						<label htmlFor="Constituency">Constituency: </label>
-						<input
-							type="text"
-							name="Constituency"
-							value={candidate.Constituency}
-							onChange={handleChange('Constituency')}
-						/>
-						<label htmlFor="Assets">Assets: </label>
-
-						<input
-							type="text"
-							name="Assets"
-							value={candidate.total_assets}
-							onChange={handleChange('total_assets')}
-						/>
-						<label htmlFor="Party">Party: </label>
-
-						<input
-							type="text"
-							name="Party"
-							value={candidate.party}
-							onChange={handleChange('party')}
-						/>
-						<label htmlFor="Education">Education: </label>
-
-						<input
-							type="text"
-							name="Education"
-							value={candidate.education}
-							onChange={handleChange('education')}
-						/>
-						<label htmlFor="CriminalCases">CriminalCases: </label>
-
-						<input
-							type="text"
-							name="CriminalCases"
-							value={candidate.criminalCases}
-							onChange={handleChange('criminalCases')}
-						/>
-					</div>
-					<div>
-						<button type="submit">update</button>
-					</div>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="updateForm">
+        <form target="#">
+          <div>
+            <div>
+              <label htmlFor="Name">Name: </label>
+              <input
+                type="text"
+                name="Name"
+                value={candidate.name}
+                onChange={handleChange("name")}
+              />
+            </div>
+            <div>
+              <label htmlFor="Constituency">Constituency: </label>
+              <input
+                type="text"
+                name="Constituency"
+                value={candidate.Constituency}
+                onChange={handleChange("Constituency")}
+              />
+            </div>
+            <div>
+              <label htmlFor="Assets">Assets: </label>
+              <input
+                type="text"
+                name="Assets"
+                value={candidate.total_assets}
+                onChange={handleChange("total_assets")}
+              />
+            </div>
+            <div>
+              <label htmlFor="Party">Party: </label>
+              <input
+                type="text"
+                name="Party"
+                value={candidate.party}
+                onChange={handleChange("party")}
+              />
+            </div>
+            <div>
+              <label htmlFor="Education">Education: </label>
+              <input
+                type="text"
+                name="Education"
+                value={candidate.education}
+                onChange={handleChange("education")}
+              />
+            </div>
+            <div>
+              <label htmlFor="CriminalCases">CriminalCases: </label>
+              <input
+                type="text"
+                name="CriminalCases"
+                value={candidate.criminalCases}
+                onChange={handleChange("criminalCases")}
+              />
+            </div>
+          </div>
+          <div>
+            <button type="submit">update</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
